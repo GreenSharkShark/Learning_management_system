91366d3690caa30987e726938c49ea8a8705f6a9
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-09-02 06:39:22 +0200

    add foreign key to Course model

--- lms/models.py
+++ lms/models.py
@@ -8,6 +8,7 @@ class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='название курса')
     preview = models.ImageField(upload_to='course_preview/', verbose_name='превью', **NULLABLE)
     description = models.TextField(verbose_name='описание', **NULLABLE)
+    lessons = models.ForeignKey('Lessons', on_delete=models.CASCADE, verbose_name='уроки курса')
 
     def __str__(self):
         return f'{self.title}'
