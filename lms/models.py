7b7b663189926135054dd2fec5496c1cb2651a0b
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-09-03 11:49:49 +0200

    task 3 lesson output field for the course model

--- lms/models.py
+++ lms/models.py
@@ -8,7 +8,6 @@ class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='название курса')
     preview = models.ImageField(upload_to='course_preview/', verbose_name='превью', **NULLABLE)
     description = models.TextField(verbose_name='описание', **NULLABLE)
-    lessons = models.ForeignKey('Lessons', on_delete=models.CASCADE, verbose_name='уроки курса')
 
     def __str__(self):
         return f'{self.title}'
@@ -22,6 +21,7 @@ class Lesson(models.Model):
     title = models.CharField(max_length=150, verbose_name='название курса')
     preview = models.ImageField(upload_to='lesson_preview/', verbose_name='превью', **NULLABLE)
     description = models.TextField(verbose_name='описание')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='курс', **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
