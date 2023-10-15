84323b9421a558489b6f548d1124524b4b952c08
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-10-15 18:26:52 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-pip install poetry
-source $(poetry env info --path)/bin/activate
-poetry install
+python3.11 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 python3.11 manage.py migrate
 python3.11 manage.py collectstatic
 deactivate
